ae683ba88b78e859e1f120b97a576bff31280a16
Dmitri Aleks <alexeyonok.dmitriy@neatsoft.io>
2022-05-04 15:33:31 +0300

    end basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-ng2';
+backgroundToggle = false
+toggle: any = false
+arr = [1,2,3,4,5]
+obj = [{
+  title: 'post1',
+  name: 'alex',
+  commet: [{
+    name: 'alex',
+    value: 'tata'
+  },
+  {
+    name: 'alex2',
+    value: 'tata2'
+  },
+  {
+    name: 'alex3',
+    value: 'tata3'
+  }]
+},
+{
+  title: 'post2',
+  name: 'alex2',
+  commet: [{
+    name: 'alex',
+    value: 'tata'
+  },
+  {
+    name: 'alex2',
+    value: 'tata2'
+  },
+  {
+    name: 'alex3',
+    value: 'tata3'
+  }]
+},
+]
 }
